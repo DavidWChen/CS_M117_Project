6e23c69c136343c2d7c01dcd1f9312091e772af2
Siddhartha Bose <siddbose@s-128-97-250-16.resnet.ucla.edu>
2017-11-13 14:24:19 -0800

    1st commit

--- Cast Me/ViewController.swift
+++ Cast Me/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var password: UILabel!
+    @IBOutlet weak var confirm: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var confirmText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
