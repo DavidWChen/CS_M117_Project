2729c0c243b16d687eefa835d8930ec45cefcc88
Vikash Singh <vikashsingh@Vikashs-MacBook-Pro.local>
2017-12-06 02:30:03 -0800

    HOORAY

--- Cast Me/friendsListViewController.swift
+++ Cast Me/friendsListViewController.swift
@@ -17,8 +17,12 @@ class friendsListViewController: UIViewController, UITableViewDelegate, UITableV
     var cleanEmail: String?
     var numfriends = -1
     @IBOutlet weak var friendsList: UITableView!
+    @IBOutlet weak var filterButton: UIButton!
+    var first = true
     
     var users = [String]()
+    var users_copy = [String]()
+    var myInterests = [String]()
     
     func readFirebase(urlstring: String) -> [String: Any] {
         var json: [String: Any]?
@@ -55,6 +59,16 @@ class friendsListViewController: UIViewController, UITableViewDelegate, UITableV
         
         friendsList.delegate = self
         friendsList.dataSource = self
+        
+        var json = readFirebase(urlstring: "user_interests/" + cleanEmail! + ".json")
+        for interest in json {
+            myInterests.append(interest.key)
+        }
+        
+        filterButton.setTitle("Filter By Interests", for: .normal)
+        friendsList.beginUpdates()
+        friendsList.reloadData()
+        friendsList.endUpdates()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -72,6 +86,18 @@ class friendsListViewController: UIViewController, UITableViewDelegate, UITableV
         let namee = json22!["name"] as! String
         cell.nameLabel.text = namee
         
+        print("reload")
+        print(filterButton.titleLabel?.text)
+        cell.contentView.backgroundColor = UIColor.white
+        if filterButton.titleLabel?.text != "Unfilter By Interests" && first == false {
+            var newinterests = users_copy.filter(){[users[indexPath.row]].contains($0)}
+            if newinterests.count > 0 {
+                cell.contentView.backgroundColor = UIColor.yellow
+            }
+        }
+        if indexPath.row == users.count - 1 {
+            first = false
+        }
         return cell
 
         /*var new_friend_email = "hi"
@@ -115,87 +141,78 @@ class friendsListViewController: UIViewController, UITableViewDelegate, UITableV
     }*/
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        users = [String]()
+        //if filterButton.titleLabel?.text == "Filter By Interests" {
+            users = [String]()
+            users_copy = [String]()
+            
+            var json0: [String: Any]?
+            json0 = readFirebase(urlstring: "gps_location.json")
+            var json1 = json0![cleanEmail!] as? [String: Any]
+            let userLat = json1!["latitude"] as! Double
+            let userLong = json1!["longitude"] as! Double
+            
+            var json2 = readFirebase(urlstring: "users.json")
+            var numFriends = json2.count - 1
+            
         
-        var json0: [String: Any]?
-        json0 = readFirebase(urlstring: "gps_location.json")
-        var json1 = json0![cleanEmail!] as? [String: Any]
-        let userLat = json1!["latitude"] as! Double
-        let userLong = json1!["longitude"] as! Double
-        
-        var json2 = readFirebase(urlstring: "users.json")
-        var numFriends = json2.count - 1
-        
-        /*var numFriends00 = 0
-        var emails = Array(repeating: "", count: numFriends00)
-        var distances = Array(repeating: 0.0, count: numFriends00)
-        var coordinates = Array(repeating: CLLocation(), count: numFriends00)
-        var json00: [String: Any]?
-        json00 = readFirebase(urlstring: "friends_list.json")
-        json00 = json00![cleanEmail!] as? [String: Any]
-        numFriends00 = json00!["friend_count"] as! Int
-        let plsemail : String = cleanEmail!*/
-        let Mycoordinate = CLLocation(latitude: userLat, longitude: userLong)
-        
-        for person in json2 {
-            if person.key != cleanEmail {
-                var jsontemp = json0![person.key] as? [String: Any]
-                let lat1 = jsontemp!["latitude"] as! Double
-                let long1 = jsontemp!["longitude"] as! Double
-                
-                if CLLocation(latitude: lat1, longitude:long1).distance(from: Mycoordinate)/1609 <= 2.0 {
-                    users.append(person.key)
-                    print(person.key)
+            let Mycoordinate = CLLocation(latitude: userLat, longitude: userLong)
+            
+            for person in json2 {
+                if person.key != cleanEmail {
+                    var jsontemp = json0![person.key] as? [String: Any]
+                    let lat1 = jsontemp!["latitude"] as! Double
+                    let long1 = jsontemp!["longitude"] as! Double
+                    
+                    if CLLocation(latitude: lat1, longitude:long1).distance(from: Mycoordinate)/1609 <= 2.0 {
+                        users.append(person.key)
+                        //print(person.key)
+                    }
                 }
             }
-        }
-        print("Nearby users: " + String(users.count))
+            print("Nearby users: " + String(users.count))
+        //} else {
+            
+            //if filter by interests
+            //var users_copy = users
+            //print(users_copy)
+            users_copy = [String]()
+            for user in users {
+                var json = readFirebase(urlstring: "user_interests/" + user + ".json")
+                var user_interests = [String]()
+                for interest in json {
+                    user_interests.append(interest.key)
+                }
+                //print(user_interests)
+                //print(myInterests)
+                var newinterests = myInterests.filter(){user_interests.contains($0)}
+                //print(newinterests)
+                if newinterests.count > 1 {
+                    users_copy.append(user)
+                }
+            }
+            print("Nearby users: " + String(users_copy.count))
+        //}
+        print(users)
+        print(users_copy)
         return users.count
         
-        /*var j = 0
-        while (j < numFriends00) //get emails
-        {
-            var json: [String: Any]?
-            json = readFirebase(urlstring: "friends_list.json")
-            json = json![cleanEmail!] as? [String: Any]
-            let k = "friend" + String(j)
-            print(k)
-            emails[j] = json![k] as! String //will fail if json![k] isnt a string
-            print(emails[j])
-            j = j+1
-            
-        }*/
-        
-        /*var i = 0
-        while (i < numFriends00) //get pin coordinates
-        {
-            var json: [String: Any]?
-            json = readFirebase(urlstring: "gps_location.json")
-            json = json![emails[i]] as? [String: Any]
-            
-            let lat = json!["latitude"] as! Double
-            print(lat)
-            let long = json!["longitude"] as! Double
-            print(long)
-            coordinates[i] = CLLocation(latitude: lat, longitude:long)
-            
-            distances[i] = coordinates[i].distance(from: Mycoordinate)/1609
-            
-            
-            i = i+1
-        }*/
-        
-        
-        /*var json: [String: Any]?
-        json = readFirebase(urlstring: "friends_list/"+plsemail+".json")
-        self.numfriends = json!.count - 1 //["friend_count"] as! Int
-        
-        print("this is numfriends: " + String(numfriends))
-        return numfriends
-        
-        var k = 0
-        var countNearFriends = 0*/
+       
     }
+    
+    @IBAction func filterClicked(_ sender: UIButton) {
+        if filterButton.titleLabel?.text == "Filter By Interests" {
+            filterButton.setTitle("Unfilter By Interests", for: .normal)
+            //filterButton.titleLabel?.text = "Unfilter By Interests"
+        } else {
+            filterButton.setTitle("Filter By Interests", for: .normal)
+            //filterButton.titleLabel?.text = "Filter By Interests"
+        }
+        friendsList.beginUpdates()
+        friendsList.reloadData()
+        friendsList.endUpdates()
+        
+    }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Create a new variable to store the instance of PlayerTableViewController
